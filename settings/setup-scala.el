2e502a8db4d5f15ae0d0e7c58576b1de1d2318c5
James Thompson <engineers+jthompson@sharethrough.com>
2015-01-11 11:55:22 -0800

    Remove redundant snippets load path

--- settings/setup-scala.el
+++ settings/setup-scala.el
@@ -1,6 +1,5 @@
 (add-hook 'scala-mode-hook
           '(lambda ()
              (yas/minor-mode-on)))
-(yas/load-directory "~/.emacs.d/elpa/yasnippet-20141223.303/snippets/scala-mode/")
 
 (provide 'setup-scala)
