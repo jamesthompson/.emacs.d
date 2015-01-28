2c4640d0ebb2949fce7a1d16712f2c6a2ab0bb9d
James Thompson <engineers+jthompson@sharethrough.com>
2015-01-27 19:10:12 -0800

    auto load scala mode for .sbt files

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -63,6 +63,7 @@
 ;; Scala
 (autoload 'scala-mode "scala-mode2")
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sbt$" . scala-mode))
 
 ;; Clojure
 (autoload 'clojure-mode "clojure-mode")
