f6f4aa7bc3f42a6ec1cc5014cfea735454440ac4
James Thompson <engineers+jthompson@sharethrough.com>
2015-02-08 11:29:18 -0800

    remove norwegian specializations

--- settings/mac.el
+++ settings/mac.el
@@ -1,6 +1,6 @@
 (require 'dash)
 
-;; change command to meta, and ignore option to use weird Norwegian keyboard
+;; change command to meta
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
