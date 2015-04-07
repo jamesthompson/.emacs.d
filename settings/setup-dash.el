6668d91622420cadd5bd92e3b9554409ff160379
James Thompson <james@orphid.com>
2015-04-07 14:31:09 -0700

    remove extraneous kbd for dash setup

--- settings/setup-dash.el
+++ settings/setup-dash.el
@@ -4,8 +4,4 @@
 (autoload 'dash-at-point "dash-at-point"
   "Search the word at point with Dash." t nil)
 
-(global-set-key "\C-cd" 'dash-at-point)
-
-(global-set-key "\C-ce" 'dash-at-point-with-docset)
-
 (provide 'setup-dash)
