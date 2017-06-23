ce03f6841b357aa2422b83c5447b87a69b6ebbe2
James Thompson <jthompson@etsemerald.com>
2017-06-23 13:21:57 -0400

    purescript alterations

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -3,11 +3,6 @@
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
 (global-set-key (kbd "C-x C-c") 'delete-frame)
 
-;; Completion that uses many different methods to find options.
-(global-set-key (kbd "C-[ [ a a") 'hippie-expand-no-case-fold)
-(global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
-(global-set-key (kbd "C-:") 'hippie-expand-lines)
-(global-set-key (kbd "C-,") 'completion-at-point)
 
 (require 'misc)
 (global-set-key (kbd "s-.") 'copy-from-above-command)
@@ -336,5 +331,10 @@
 
 ;; Indent region
 (global-set-key (kbd "C-i") 'indent-region)
+
+;; Completion that uses many different methods to find options.
+(global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
+(global-set-key (kbd "C-:") 'hippie-expand-lines)
+(global-set-key (kbd "C-,") 'completion-at-point)
 
 (provide 'key-bindings)
